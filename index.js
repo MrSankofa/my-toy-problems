2cea3aaca6e2ddbec3047d97fbca6fc96ab6556e
brett.cunningham <brett.cunningham@sbevision.com>
2024-11-29 13:35:40 -0500

    chore(js/ts): classic

---
+++ index.js
@@ -0,0 +1,12 @@
+const fizzBuzz = (arr) => {
+
+  for (const num of arr) {
+    if (num % 3 === 0 && num % 5 === 0){
+      console.log('fizzbuzz')
+    } else if(num % 3 === 0){
+      console.log('fizz')
+    } else if(num % 5 === 0){
+      console.log('buzz')
+    } 
+  }
+}
