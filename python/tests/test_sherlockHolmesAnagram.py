7b4ccd0e5880bcbc62408a29811bddba32f975b2
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-30 13:45:09 -0500

    fix(test): python tests are fixed now

--- python/tests/test_sherlockHolmesAnagram.py
+++ python/tests/test_sherlockHolmesAnagram.py
@@ -4,7 +4,7 @@ from src.problems.SherlockHomesAnagrams import sherlock_homes_find_anagrams
 
 class TestSherlockHolmes(unittest.TestCase):
     def test_abba(self):
-        self.assertEqual(sherlock_homes_find_anagrams("abba"), 20)
+        self.assertEqual(sherlock_homes_find_anagrams("abba"), 4)
 
     def test_abcd(self):
         self.assertEqual(sherlock_homes_find_anagrams("abcd"), 0)
