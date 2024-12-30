7a3b183cec841cedec16fd5e6436a803e80d14a7
brett.cunningham <brett.cunningham@sbevision.com>
2024-12-30 13:43:40 -0500

    wip(test): the test names were incorrect but this should still fail in the github action

--- python/tests/test_sherlockHolmesAnagram.py
+++ python/tests/test_sherlockHolmesAnagram.py
@@ -4,18 +4,18 @@ from src.problems.SherlockHomesAnagrams import sherlock_homes_find_anagrams
 
 class TestSherlockHolmes(unittest.TestCase):
     def test_abba(self):
-        self.assertEqual(sherlock_homes_find_anagrams("abba"), 5)
+        self.assertEqual(sherlock_homes_find_anagrams("abba"), 20)
 
-    def test_abba(self):
+    def test_abcd(self):
         self.assertEqual(sherlock_homes_find_anagrams("abcd"), 0)
 
-    def test_abba(self):
+    def test_ifailuhkqq(self):
         self.assertEqual(sherlock_homes_find_anagrams("ifailuhkqq"), 3)
 
-    def test_abba(self):
+    def test_cdcd(self):
         self.assertEqual(sherlock_homes_find_anagrams("cdcd"), 5)
 
-    def test_abba(self):
+    def test_kkkk(self):
         self.assertEqual(sherlock_homes_find_anagrams("kkkk"), 10)
 
 if __name__ == "__main__":
